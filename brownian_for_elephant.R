6e942f1a0c35f2bab6ef8d085083e0366bc875c0
pletenev <apletenev1@yandex.ru>
2016-05-06 17:45:54 +0300

    Added files via upload

---
+++ brownian_for_elephant.R
@@ -0,0 +1,19 @@
+#######для Fly Elephant######
+install.packages("adehabitatLT")
+install.packages("adehabitatHR")
+require("adehabitatLT")
+require("adehabitatHR")
+
+all_6_from_rt_adj_2<-dget(file='~/workdir/all_6_from_rt_adj_2')
+###загружаю траектории
+all.traj_adj<-as.ltraj(xy = all_6_from_rt_adj_2[,c("x.smooth","y.smooth")], 
+                date = all_6_from_rt_adj_2$date_time,id=as.character(all_6_from_rt_adj_2$id)) 
+
+kernel_Brownian_list<-list()
+a<-as.numeric()
+for (i in seq(0.5,2.5,by=0.5)) {
+a=a+1
+kernel_Brownian_list[[a]]<-kernelbb(all.traj_adj, sig1=i, sig2=15, grid = 900, same4all = TRUE, 
+  byburst = TRUE,extent = 0.1, nalpha = 25)
+}
+save(kernel_Brownian_list, file='~/workdir/kernel_Brownian_list')
