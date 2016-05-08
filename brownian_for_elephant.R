6bee06f6d813ed983b47db2f0e48e66b6178c733
Dmitry Spodarets <d.spodarets@flyelephant.net>
2016-05-08 11:08:23 +0300

    Update brownian_for_elephant.R

--- brownian_for_elephant.R
+++ brownian_for_elephant.R
@@ -1,13 +1,18 @@
 #######äëÿ Fly Elephant######
 install.packages('sp', repos='http://cran.us.r-project.org', lib= "~/")
 install.packages('ade4', repos='http://cran.us.r-project.org', lib= "~/")
+install.packages('adehabitatMA', repos='http://cran.us.r-project.org', lib= "~/")
+install.packages('CircStats', repos='http://cran.us.r-project.org', lib= "~/")
+install.packages('deldir', repos='http://cran.us.r-project.org', lib= "~/")
 install.packages('adehabitatHR', repos='http://cran.us.r-project.org', lib= "~/")
 install.packages('adehabitatLT', repos='http://cran.us.r-project.org', lib= "~/")
 library("sp",lib.loc = "~/")
 library("ade4",lib.loc = "~/")
+library("adehabitatMA",lib.loc = "~/")
+library("CircStats",lib.loc = "~/")
+library("deldir",lib.loc = "~/")
 library("adehabitatLT",lib.loc = "~/")
 library("adehabitatHR",lib.loc = "~/")
-
 
 all_6_from_rt_adj_2<-dget(file='~/workdir/all_6_from_rt_adj_2')
 ###çàãðóæàþ òðàåêòîðèè
