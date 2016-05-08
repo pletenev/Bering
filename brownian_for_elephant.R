2c21bafe121fa5ed0f09f5072c0d870c8a743ac1
Dmitry Spodarets <d.spodarets@flyelephant.net>
2016-05-08 09:42:17 +0300

    Update brownian_for_elephant.R

--- brownian_for_elephant.R
+++ brownian_for_elephant.R
@@ -1,10 +1,13 @@
 #######äëÿ Fly Elephant######
 install.packages('sp', repos='http://cran.us.r-project.org', lib= "~/")
+install.packages('ade4', repos='http://cran.us.r-project.org', lib= "~/")
 install.packages('adehabitatHR', repos='http://cran.us.r-project.org', lib= "~/")
 install.packages('adehabitatLT', repos='http://cran.us.r-project.org', lib= "~/")
 library("sp",lib.loc = "~/")
 library("adehabitatLT",lib.loc = "~/")
 library("adehabitatHR",lib.loc = "~/")
+library("ade4",lib.loc = "~/")
+
 
 all_6_from_rt_adj_2<-dget(file='~/workdir/all_6_from_rt_adj_2')
 ###çàãðóæàþ òðàåêòîðèè
